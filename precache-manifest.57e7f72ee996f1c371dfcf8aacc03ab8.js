5144a52464cc25c91ba73c4ca7d503af65e52703
Harsh Tibrewal <harshtibrewal18@gmail.com>
2020-07-14 11:57:05 +0530

    Updates

---
+++ precache-manifest.57e7f72ee996f1c371dfcf8aacc03ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b5fc355bc931fab01603f3fd44b906",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "5a2412d01992e5788af2",
+    "url": "/react-tic-tac-toe/static/css/main.e7158266.chunk.css"
+  },
+  {
+    "revision": "dcd786cfa4d728609971",
+    "url": "/react-tic-tac-toe/static/js/2.8b338b58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.8b338b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2412d01992e5788af2",
+    "url": "/react-tic-tac-toe/static/js/main.9bf2d79b.chunk.js"
+  },
+  {
+    "revision": "929d476f7b1ee9149c3a",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.db7cfeb9.js"
+  }
+]);
